45dd8b2895b48d738490f891cf5c7caf08839e2b
Robbie <robbie331@>
2024-07-24 16:00:00 +0900

    1-5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,8 +5,9 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        while (true) {
+        int cnt = 0;
+        int[] results = new int[10];
+        while(true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int x = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
@@ -21,25 +22,33 @@ public class App {
                 case '+':
                     result = x + y;
                     System.out.println("결과: " + result);
+                    results[cnt] = result;
+                    cnt++;
                     break;
                 case '-':
                     result = x - y;
                     System.out.println("결과: " + result);
+                    results[cnt] = result;
+                    cnt++;
                     break;
                 case '*':
                     result = x * y;
                     System.out.println("결과: " + result);
+                    results[cnt] = result;
+                    cnt++;
                     break;
                 case '/':
                     if (y != 0) {
                         result = x / y;
                         System.out.println("결과: " + result);
+                        results[cnt] = result;
+                        cnt++;
                     } else System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     break;
             }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String doorstop = sc.next();
-            if (doorstop.equals("exit")) break;
+            if(doorstop.equals("exit")) break;
         }
     }
 }
