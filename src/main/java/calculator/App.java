aecef6055268b7d48ad855f9f20ed7b62f8f891f
Robbie <robbie331@>
2024-07-24 15:58:00 +0900

    1-4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,35 +6,40 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int x = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int y = sc.nextInt();
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int x = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int y = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char z = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char z = sc.next().charAt(0);
 
-        int result = 0;
-        switch(z) {
-            case '+' :
-                result = x + y;
-                System.out.println("결과: " + result);
-                break;
-            case '-' :
-                result = x - y;
-                System.out.println("결과: " + result);
-                break;
-            case '*' :
-                result = x * y;
-                System.out.println("결과: " + result);
-                break;
-            case '/' :
-                if(y != 0) {
-                    result = x / y;
+            int result = 0;
+
+            switch (z) {
+                case '+':
+                    result = x + y;
                     System.out.println("결과: " + result);
-                }
-                else System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                break;
+                    break;
+                case '-':
+                    result = x - y;
+                    System.out.println("결과: " + result);
+                    break;
+                case '*':
+                    result = x * y;
+                    System.out.println("결과: " + result);
+                    break;
+                case '/':
+                    if (y != 0) {
+                        result = x / y;
+                        System.out.println("결과: " + result);
+                    } else System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+            }
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String doorstop = sc.next();
+            if (doorstop.equals("exit")) break;
         }
     }
 }
