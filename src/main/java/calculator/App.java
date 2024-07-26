59d44505fd2a11f0e1bc9a284c5cfbd30a901a03
Robbie <robbie331@>
2024-07-26 20:17:31 +0900

    2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -9,35 +10,57 @@ public class App {
     public static void main(String[] args) {
 
         Queue<Integer> que1 = new LinkedList<Integer>();
+        ArrayList<Double> que2 = new ArrayList<Double>();
+
         Calculator cal1 = new Calculator(que1);
+        Calculator cal2 = new Calculator(que2);
+        double radius = 0;
 
         Scanner sc = new Scanner(System.in);
-
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int x = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int y = sc.nextInt();
+            int witchthing = 0;
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char z = sc.next().charAt(0);
+            System.out.println("원의 넓이 구하기 : 1을 입력, 계산기 기능 이용하기 : 2를 입력");
+            witchthing = sc.nextInt();
 
-            int result = 0;
+            if (witchthing == 1) {
+                System.out.println("반지름 길이를 입력하세요.");
+                radius = sc.nextInt();
+                cal2.calculateCircleArea(radius);
 
-            result = cal1.calculate(z, x, y);
-            System.out.println(result);
+                cal2.inquiryCircleArea();
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String removeornot = sc.next();
-            if (removeornot.equals("remove")) cal1.removeResult();
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String doorstop = sc.next();
+                if (doorstop.equals("exit")) break;
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiryornot = sc.next();
-            if (inquiryornot.equals("inquiry")) cal1.inquiryResults();
+            } else if (witchthing == 2) {
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String doorstop = sc.next();
-            if (doorstop.equals("exit")) break;
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int x = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int y = sc.nextInt();
+
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char z = sc.next().charAt(0);
+
+                int result = 0;
+
+                result = cal1.calculate(z, x, y);
+                System.out.println(result);
+
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String removeornot = sc.next();
+                if (removeornot.equals("remove")) cal1.removeResult();
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiryornot = sc.next();
+                if (inquiryornot.equals("inquiry")) cal1.inquiryResults();
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String doorstop = sc.next();
+                if (doorstop.equals("exit")) break;
+            }
         }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,15 +1,24 @@
 package calculator;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Calculator {
 
-    private Queue<Integer> calculator;
+    //static, final을 사용하지 않은 이유는 계산기 기능과 원의 넓이를 구하는 과정에서, 값이 하나이며 반드시 변하지 않는 상수는 원의 넓이를 구하는 식에 쓰는 PI가 있다고 판단했지만
+    //Math클래스 기능을 사용하면 굳이 static final을 쓸 필요가 없다고 생각하여 쓰지 않았습니다.
 
-    Calculator(Queue<Integer> calculator)
-    {
+    private Queue<Integer> calculator;
+    private ArrayList<Double> calculatorcircle;
+
+    Calculator(Queue<Integer> calculator) {
         this.calculator = calculator;
+    }
+
+    Calculator(ArrayList<Double> calculator) {
+        this.calculatorcircle = calculator;
     }
 
     public Queue<Integer> getCalculator() {
@@ -26,6 +35,22 @@ public class Calculator {
 
     public void inquiryResults() {
         for (int a : this.getCalculator()) System.out.println(a);
+    }
+
+    public Double calculateCircleArea(Double radius) {
+        calculatorcircle.add(radius * radius * Math.PI);
+        return radius * radius * Math.PI;
+    }
+
+    public ArrayList<Double> getCalculatorcircle() {
+        return calculatorcircle;
+    }
+
+    public void setCalculatorcircle() {
+    }
+
+    public void inquiryCircleArea() {
+        for (double a : this.getCalculatorcircle()) System.out.println(a);
     }
 
     public int calculate(char operator, int firstNumber, int secondNumber) {
